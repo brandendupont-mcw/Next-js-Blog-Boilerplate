9fc018d01ab082ef9799159d8ac27044f7de2173
brandendupont-mcw <bdupont@mcw.edu>
2022-06-13 01:35:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const baseUrl = '/';
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '/pfa/',
+  basePath: '/pfa',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
